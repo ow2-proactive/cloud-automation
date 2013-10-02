e302dc432645e05f9d9494aeb20dbfa284987329
Mauricio Jost <mauricio.jost@activeeon.com>
2013-10-02 14:52:28 +0200

    Added ActionExecutor.
    Signed-off-by: Mauricio Jost <mauricio.jost@activeeon.com>

---
+++ src/main/java/org/ow2/proactive/brokering/triggering/ActionExecutor.java
@@ -0,0 +1,36 @@
+package org.ow2.proactive.brokering.triggering;
+
+import org.apache.log4j.Logger;
+import org.ow2.proactive.brokering.occi.infrastructure.ActionTrigger;
+import java.util.Map;
+
+public class ActionExecutor extends Thread {
+
+    private static final Logger logger = Logger.getLogger(ActionExecutor.class.getName());
+
+    private Class action;
+    private Map<String, String> args;
+
+    public ActionExecutor(Map<String, String> args) throws ScriptException {
+        this.args = args;
+        this.action = ScriptUtils.getEncodedScriptAsClass(args, ActionTrigger.OCCI_MONITORING_ACTION);
+    }
+
+    private void executeAction(Class script) {
+        if (script == null)
+            return;
+
+        try {
+            Action action = (Action) script.newInstance();
+            action.execute(args);
+        } catch (Throwable e) {
+            logger.warn("Error executing action: " + script, e);
+        }
+    }
+
+    @Override
+    public void run() {
+        executeAction(action);
+    }
+
+}
