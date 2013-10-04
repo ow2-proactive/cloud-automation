095adcd004779cd6cdcc93587a2ed103d8ed6a09
Mauricio Jost <mauricio.jost@activeeon.com>
2013-10-04 20:06:16 +0200

    Cloudwatt platform configuration parameters.

--- src/main/resources/config/rules/cloudwatt.groovy
+++ src/main/resources/config/rules/cloudwatt.groovy
@@ -11,27 +11,21 @@ public class Cloudwatt implements Rule {
 
         attributes.put("occi.compute.vendor.name", "OPENSTACK");
         attributes.put("endpoint", "***REMOVED***");
-        attributes.put("login", "admin");
-        attributes.put("password", "activeeon");
-        attributes.put("occi_endpoint", "***REMOVED***");
-        attributes.put("utils.scripts.server.address", "http://10.0.0.2:8000");
         attributes.put("iaas.provider.name", "NOVA");
-        attributes.put("iaas.provider.api.url", "http://node4.cloud.sophia.inria.fr:5000/v2.0");
-        attributes.put("iaas.provider.api.user", "admin");
+        attributes.put("iaas.provider.api.url", "***REMOVED***");
+        attributes.put("iaas.provider.api.user", "mjost");
         attributes.put("iaas.provider.api.password", "***REMOVED***");
-        attributes.put("iaas.provider.api.tenant", "Admin");
-        attributes.put("iaas.provider.newvm.image", "fd5c12dc-10e4-4a7f-b984-f221d848af07");
-        attributes.put("iaas.provider.newvm.flavor", "2");
-        attributes.put("iaas.provider.newvm.java.home.dir", "/home/ubuntu/opt/jdk/");
-        attributes.put("iaas.provider.newvm.proactive.home.dir", "/home/ubuntu/opt/proactive/latest/scheduling/");
-        attributes.put("proactive.rm.url", "pamr://0/");
-        attributes.put("proactive.rm.credentials", "UlNBCjEwMjQKUlNBL0VDQi9QS0NTMVBhZGRpbmcKh7ucJr4s5kcMLNplKvoQMyq0JGlDQNbJTqmWuyTA2U9gJYACHZfTME6orv5837HUAYQjLLsASq3BV8O0Y1T/MoRyZ6hOGzFG26u4hrZHNNr+IxvHbA0BKeju2XW6kE/Tbjz2eFHrogCnrAJHE5G4hrp6B6AkVVZoqzIc5R+f/je6j1/ZK+f+4erc0FWSH6DcxGG1nHZeAyue/7digDw5+PbnZIg3w9yZVe/b7nfN0Ablh0B8DW4Tqqlvxhc+/RPQVwnXNw62M6NUzO9beQMY4EQFVhJc1FHBU+RhNVzku90gXkqPvi/nc1TE0qZli/OQhJQTWYqqhmBOjVX3eVm92FOWT2UWZVkh7ll2vSjxdPYqKI2jng1mw3etbYGlOUKPZtOuTuS1mAswh47k/wZBfQ==");
+        attributes.put("iaas.provider.api.tenant", "***REMOVED***");
+        attributes.put("iaas.provider.newvm.image", "***REMOVED***");
+        attributes.put("iaas.provider.newvm.flavor", "18");
+        attributes.put("iaas.provider.newvm.java.home.dir", "/opt/jdk1.6.0_25/");
+        attributes.put("iaas.provider.newvm.proactive.home.dir", "/opt/scheduling/");
+        attributes.put("proactive.rm.url", "pnp://194.2.202.227:64738/");
+        attributes.put("proactive.rm.credentials", "***REMOVED***");
         attributes.put("proactive.rm.nodesource.name", "OPEN_STACK_MONITORING");
-        attributes.put("utils.scripts.server.address", "http://10.0.0.2:8000");
-        attributes.put("utils.scripts.rscripts.path", "/home/ubuntu/utils/rscript.sh");
-        attributes.put("utils.script.relativepath", "general/show-info.sh");
-        attributes.put("iaas.provider.newvm.name", "vmnamev002");
 
         return attributes;
+
     }
+
 }
